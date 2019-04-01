73c00b808555a360ee42bb875f1686c7dae233cb
onHawk <luisd824@gmail.com>
2019-04-01 17:35:56 -0400

    create a home screen

---
+++ quicknews/screens/Homescreen.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
+
+class Homescreen extends Component {
+  state = {};
+  render() {
+    return (
+      <View>
+        <Text>Homescreen</Text>
+      </View>
+    );
+  }
+}
+
+export default Homescreen;
