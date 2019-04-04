bbdbc980dce7233f030b9ca9f4171c2652ec4088
onHawk <luisd824@gmail.com>
2019-04-03 21:30:33 -0400

    fetch news from api

--- quicknews/screens/Homescreen.js
+++ quicknews/screens/Homescreen.js
@@ -2,11 +2,38 @@ import React, { Component } from 'react';
 import { StyleSheet, Text, View, Button } from 'react-native';
 
 class Homescreen extends Component {
-  state = {};
+  // constructor(props) {
+  //   super(props);
+  // }
+  state = {
+    articles: [],
+  };
+
+  componentDidMount() {
+    this.fetchNews();
+  }
+
+  fetchNews() {
+    const url =
+      'https://newsapi.org/v2/top-headlines?' +
+      'country=us&' +
+      'apiKey=ef49c93e09c542b98132457198e197ed';
+
+    const req = new Request(url);
+    return fetch(req)
+      .then(response => response.json())
+      .then(promise => {
+        this.setState({ articles: promise.articles });
+      });
+  }
   render() {
+    console.log(this.state.articles);
     return (
       <View>
         <Text>Homescreen</Text>
+        <Button title="get news" onPress={() => this.fetchNews}>
+          Get
+        </Button>
       </View>
     );
   }
