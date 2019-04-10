d15a90ff2bdab7cbaf9891790ee67ee78a64e79a
onHawk <luisd824@gmail.com>
2019-04-09 23:11:14 -0400

    add card component to display news

--- quicknews/components/Articles.js
+++ quicknews/components/Articles.js
@@ -2,13 +2,35 @@ import React, { Component } from 'react';
 
 import { StyleSheet, Text, View, Button } from 'react-native';
 
+import {
+  Content,
+  Card,
+  CardItem,
+  Thumbnail,
+  Icon,
+  Left,
+  Body,
+  Right,
+} from 'native-base';
+
 class Articles extends Component {
   render() {
+    const { title, description, url } = this.props.article;
     console.log(this.props.articles);
     return (
-      <View>
-        <Text>something</Text>
-      </View>
+      <Content>
+        <Card>
+          <CardItem>
+            <Text>{title}</Text>
+          </CardItem>
+          <CardItem>
+            <Text>{description}</Text>
+          </CardItem>
+          <CardItem>
+            <Text>{url}</Text>
+          </CardItem>
+        </Card>
+      </Content>
     );
   }
 }
