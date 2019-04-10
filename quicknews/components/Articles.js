e31a9627bfe27ce037e693d6f4e1637c6d6cce61
onHawk <luisd824@gmail.com>
2019-04-10 16:34:51 -0400

    add more information to article card

--- quicknews/components/Articles.js
+++ quicknews/components/Articles.js
@@ -1,6 +1,13 @@
 import React, { Component } from 'react';
 
-import { StyleSheet, Text, View, Button } from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  View,
+  Button,
+  TouchableOpacity,
+  Linking,
+} from 'react-native';
 
 import {
   Content,
@@ -15,8 +22,16 @@ import {
 
 class Articles extends Component {
   render() {
-    const { title, description, url } = this.props.article;
-    console.log(this.props.articles);
+    const {
+      title,
+      description,
+      publishedAt,
+      source,
+      urlToImage,
+      url,
+    } = this.props.article;
+
+    // console.log(this.props.articles);
     return (
       <Content>
         <Card>
@@ -24,10 +39,23 @@ class Articles extends Component {
             <Text>{title}</Text>
           </CardItem>
           <CardItem>
-            <Text>{description}</Text>
+            <Thumbnail style={styles.articleImg} source={{ uri: urlToImage }} />
           </CardItem>
           <CardItem>
-            <Text>{url}</Text>
+            <Body>
+              <Text>{description}</Text>
+            </Body>
+          </CardItem>
+          <CardItem>
+            <Body>
+              <Text style={styles.source}>{source.name}</Text>
+              <TouchableOpacity
+                style={styles.article}
+                onPress={() => Linking.openURL(url)}
+              >
+                <Text>Full Article</Text>
+              </TouchableOpacity>
+            </Body>
           </CardItem>
         </Card>
       </Content>
@@ -35,4 +63,18 @@ class Articles extends Component {
   }
 }
 
+const styles = StyleSheet.create({
+  article: {
+    backgroundColor: '#DDDDDD',
+    padding: 8,
+  },
+  articleImg: {
+    flex: 1,
+    height: 200,
+    borderRadius: 3,
+  },
+  source: {
+    color: '#1E90FF',
+  },
+});
 export default Articles;
