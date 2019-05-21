522a600f6e7e530e8b2744768d12fe12640c4c06
onHawk <luisd824@gmail.com>
2019-05-20 21:45:56 -0400

    create cnn screen

---
+++ quicknews/screens/Cnn.js
@@ -0,0 +1,78 @@
+import React, { Component } from 'react';
+import { StyleSheet, Text, View, FlatList } from 'react-native';
+
+import {
+  Button,
+  Container,
+  Header,
+  Title,
+  Left,
+  Icon,
+  Right,
+  Body,
+  Content,
+} from 'native-base';
+
+import Articles from '../components/Articles';
+
+class Cnn extends Component {
+  state = {
+    articles: [],
+    refresh: true,
+  };
+
+  componentDidMount() {
+    this.fetchNews();
+  }
+
+  fetchNews() {
+    const url =
+      'https://newsapi.org/v2/everything?' +
+      'domains=cnn.com&' +
+      'apiKey=ef49c93e09c542b98132457198e197ed';
+
+    const req = new Request(url);
+    return fetch(req)
+      .then(response => response.json())
+      .then(promise => {
+        console.log(promise.articles);
+        this.setState({
+          articles: promise.articles,
+          refresh: !this.state.refresh,
+        });
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }
+
+  render() {
+    return (
+      <Container>
+        <Header transparent>
+          <Left>
+            <Button
+              transparent
+              onPress={() => this.props.navigation.openDrawer()}
+            >
+              <Icon name="menu" style={{ color: 'black' }} />
+            </Button>
+          </Left>
+          <Body>
+            <Title style={{ color: 'black' }}>CNN</Title>
+          </Body>
+        </Header>
+        <FlatList
+          data={this.state.articles}
+          renderItem={({ item }) => (
+            <Articles article={item} navigation={this.props.navigation} />
+          )}
+          refreshing={this.state.refresh}
+          onRefresh={() => this.toRefresh}
+        />
+      </Container>
+    );
+  }
+}
+
+export default Cnn;
